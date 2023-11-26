0a03dd783e271f7398e7e155fe8c1b0c258c105d
Chi Onabolu <chionabolu@gmail.com>
2023-11-26 14:36:43 -0600

    added first question and congratulation statements

--- quizgame.py
+++ quizgame.py
@@ -1,6 +1,19 @@
-print("Welcome to my Amazing quiz!")
+print("Welcome to my Python Basics quiz!")
 
 playing = input("Do you want to play? ")
 
-if playing != "yes":
+if playing.lower() != "yes":
     quit()
+
+print("Okay! Let's play <3")
+score = 0
+
+answer = input("What symbol is used to comment out a line? ")
+if answer.lower() == "#":
+    print('Correct!')
+    score += 1
+else: 
+    print("Sorry, that was wrong!")
+
+print("You got " + str(score) + " questions correct!")
+print("You got " + str(score / 4) + "%.")
